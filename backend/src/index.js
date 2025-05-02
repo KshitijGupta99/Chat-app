27097dffe751a7471b98dff49a7daf2c2415850e
Kshitij Gupta <144701934+KshitijGupta99@users.noreply.github.com>
2025-05-02 18:08:46 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));  
 app.use(express.urlencoded({ limit: "50mb", extended: true }));  
 
